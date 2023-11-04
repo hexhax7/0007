4787e5e6f75a0ab005c61f034a7ebec32ccc3f8f
hexhax7 <87332576+hexhax7@users.noreply.github.com>
2023-11-04 13:29:00 +0000

    triple0g

--- app/page.js
+++ app/page.js
@@ -1,95 +1,69 @@
 import Image from 'next/image'
-import styles from './page.module.css'
+import Contact from './contact/page';
+import { Abel } from 'next/font/google';
+
+const abel = Abel({ subsets: ['latin'], weight: "400"})
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="main">
+      <div className='logo'>
+        <div><h1>H</h1></div>
+        <h2 className={abel.className} style={{fontSize: "32px"}}>hexhax</h2><h2 className={abel.className}>Drones</h2>
+        
+      </div>
+              <Image
+
+          src="/DJI_0465.jpg"
+          style={{ objectFit: "contain", width: "100vw", height: "100%" }}
+          width={4000}
+          height={2250}
+          
+          
+        />
+
+
+      <div className="specs">
+        <h2 >12MP photos</h2>
+        <div ></div>
+        <h2 >4K/120fps</h2>
+      </div>
+
+      <div className='chamonix'>
+        <h1 className={abel.className}>Chamonix 19/09/2023</h1>
+        <div className='chamonix-flex'>
+          <div><Image src="/DJI_0578.jpg" className="chamonix-img" width={4000} height={2250} style={{objectFit:"contain"}}
+          /></div>
+        
+           <video autoPlay loop muted width="4000" height="386" style={{objectFit:"contain"}}>
+            <source src="/Video2.mp4" type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
         </div>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
+      <div className="services">
+        <div>
+        <Image src="/Drone_inspection.png" width={512} height={512} style={{objectFit:"contain"}} />
+          <p className="">
+            Explore drone inspections for construction sites - they excel at accessing challenging areas, ensuring thorough, safe assessments. Rely on precision and ease, eliminating risky manual inspections for confident decision-making.
           </p>
-        </a>
+        </div>
+      
+        <div>
+          <Image src="/Drone_imagery.png" width={512} height={512} style={{objectFit:"contain"}} />
+          <p className="">
+            Elevate your website with our drone photography service. We capture high-quality, unique aerial shots that offer a fresh perspective on your work. Transform your online presence with striking visuals.
+          </p>
+        </div>
+        </div>
+        <div className="gallery">
+              <Image className="m-5" src="/DJI_0539.jpg" style={{ objectFit: "contain", width: "90vw", height: "100%" }} width={4000} height={2000}
+                   />
+              <Image className="m-5" src="/DJI_0524.jpg" style={{ objectFit: "contain", width: "90vw", height: "100%" }} width={4000} height={2000}
+                   />
       </div>
+      <Contact />
+
     </main>
-  )
+  );
 }
