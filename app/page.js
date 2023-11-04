bd83b24a0cc2f9a9460618158cdd644c4d9a7a2f
hexhax7 <87332576+hexhax7@users.noreply.github.com>
2023-11-04 18:14:25 +0000

    good app update

--- app/page.js
+++ app/page.js
@@ -1,42 +1,88 @@
-import Image from 'next/image'
-import Contact from './contact/page';
-import { Abel } from 'next/font/google';
+import Image from "next/image";
+import Contact from "./contact/page";
+import { Abel } from "next/font/google";
 
-const abel = Abel({ subsets: ['latin'], weight: "400"})
+const abel = Abel({ subsets: ["latin"], weight: "400" });
 
 export default function Home() {
   return (
     <main className="main">
-      <div className='logo'>
-        <div><h1>H</h1></div>
-        <h2 className={abel.className} style={{fontSize: "32px"}}>hexhax</h2><h2 className={abel.className}>Drones</h2>
-        
+      <div className="logo">
+        <div>
+          <h1>H</h1>
+        </div>
+        <h2 className={abel.className} style={{ fontSize: "32px" }}>
+          hexhax
+        </h2>
+        <h2 className={abel.className}>Drones</h2>
       </div>
-              <Image
-
-          src="/DJI_0465.png"
-          style={{ objectFit: "contain", width: "100vw",  height: "100%" }}
-          
-          width={4000}
-          loading="lazy" height={2250}
-          
-          
+      <Image
+        className="landscape"
+        src="/DJI_0465.png"
+        style={{ objectFit: "cover" }}
+        width={4000}
+        height={2250}
+        layout="responsive"
+      />
+      <Image
+        src="/portrait/DJI_04669x16.png"
+        style={{ objectFit: "contain", width: "100%", height: "auto" }}
+        className="portrait"
+        width={1266}
+        loading="lazy"
+        height={2250}
+      />
+      <div className="specs-flex">
+        <Image
+          className="specs-flex-image"
+          style={{ objectFit: "contain" }}
+          src="/djimini2.jpg"
+          width={2000}
+          height={1125}
         />
+        <div className="specs-text">
+          <p>
+            Elevate Your Construction and Commercial Visuals Discover the
+            potential of our drone services, tailored to enhance your
+            construction and commercial imagery. With extended flight
+            capabilities, reliable wind resistance, long-range video
+            transmission, and the ability to capture 12MP photos and 4K/120fps
+            video, our drones offers a valuable tool for your visual projects.
+            Contact us today to explore how we can assist you in capturing
+            captivating content.
+          </p>
 
-
-      <div className="specs">
-        <h2 >12MP photos</h2>
-        <div ></div>
-        <h2 >4K/120fps</h2>
+          <div className="specs">
+            <h2>12MP photos</h2>
+            <div></div>
+            <h2>4K/120fps</h2>
+          </div>
+        </div>
       </div>
-
-      <div className='chamonix'>
+      ?
+      <div className="chamonix">
         <h1 className={abel.className}>Chamonix 19/09/2023</h1>
-        <div className='chamonix-flex'>
-          <div><Image src="/DJI_0578.png" className="chamonix-img" width={4000} loading="lazy" height={2250} style={{objectFit:"contain"}}
-          /></div>
-        
-           <video autoPlay loop muted width="4000" loading="lazy" height="386" style={{objectFit:"contain"}}>
+        <div className="chamonix-flex">
+          <div>
+            <Image
+              src="/DJI_0578.png"
+              className="chamonix-img"
+              width={4000}
+              loading="lazy"
+              height={2250}
+              style={{ objectFit: "contain" }}
+            />
+          </div>
+
+          <video
+            autoPlay
+            loop
+            muted
+            width="4000"
+            loading="lazy"
+            height="386"
+            style={{ objectFit: "contain" }}
+          >
             <source src="/Video2.mp4" type="video/mp4" />
             Your browser does not support the video tag.
           </video>
@@ -44,27 +90,57 @@ export default function Home() {
       </div>
       <div className="services">
         <div>
-        <Image src="/Drone_inspection.png" width={512} loading="lazy" height={512} style={{objectFit:"contain"}} alt='drone inspection' />
+          <Image
+            src="/Drone_inspection.png"
+            width={512}
+            loading="lazy"
+            height={512}
+            style={{ objectFit: "contain" }}
+            alt="drone inspection"
+          />
           <p className="">
-            Explore drone inspections for construction sites - they excel at accessing challenging areas, ensuring thorough, safe assessments. Rely on precision and ease, eliminating risky manual inspections for confident decision-making.
+            Explore drone inspections for construction sites - they excel at
+            accessing challenging areas, ensuring thorough, safe assessments.
+            Rely on precision and ease, eliminating risky manual inspections for
+            confident decision-making.
           </p>
         </div>
-      
+
         <div>
-          <Image src="/Drone_imagery.png" width={512} loading="lazy" height={512} style={{objectFit:"contain"}} alt='drone imagery'/>
+          <Image
+            src="/Drone_imagery.png"
+            width={512}
+            loading="lazy"
+            height={512}
+            style={{ objectFit: "contain" }}
+            alt="drone imagery"
+          />
           <p className="">
-            Elevate your website with our drone photography service. We capture high-quality, unique aerial shots that offer a fresh perspective on your work. Transform your online presence with striking visuals.
+            Elevate your website with our drone photography service. We capture
+            high-quality, unique aerial shots that offer a fresh perspective on
+            your work. Transform your online presence with striking visuals.
           </p>
         </div>
-        </div>
-        <div className="gallery">
-              <Image className="m-5" src="/DJI_0539.png" style={{ objectFit: "contain", width: "90vw",  height: "100%" }}  width={4000} loading="lazy" height={2000}
-                   />
-              <Image className="m-5" src="/DJI_0524.png" style={{ objectFit: "contain", width: "90vw",  height: "100%" }}  width={4000} loading="lazy" height={2000}
-                   />
+      </div>
+      <div className="gallery">
+        <Image
+          className="m-5"
+          src="/DJI_0539.png"
+          style={{ objectFit: "contain", width: "90vw", height: "100%" }}
+          width={4000}
+          loading="lazy"
+          height={2000}
+        />
+        <Image
+          className="m-5"
+          src="/DJI_0524.png"
+          style={{ objectFit: "contain", width: "90vw", height: "100%" }}
+          width={4000}
+          loading="lazy"
+          height={2000}
+        />
       </div>
       <Contact />
-
     </main>
   );
 }
